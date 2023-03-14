a31296bb4dea51f07be1732340234d55ced027d7
qd-qd <qdqdqdqdqd@protonmail.com>
2023-03-14 17:47:34 +0100

    🔧 improve the configuration of checkpoints
    - Implement the template that will allow us to follow emitted events from deployed accounts
    - Fix the ABI configuration. Thanks to that writers will receive parsed events

--- src/config.ts
+++ src/config.ts
@@ -1,26 +1,25 @@
-import { CheckpointConfig } from "@snapshot-labs/checkpoint";
-import accountABI from './abis/accountABI.json';
-import deployerABI from './abis/deployerABI.json';
+import { CheckpointConfig } from '@snapshot-labs/checkpoint';
+
+// TODO: this one should be dynamic
+const STARTING_BLOCK = 779875;
 
 const config: CheckpointConfig = {
-    network_node_url: process.env.RPC_URL as string,
-    sources: [
-        {
-            contract: process.env.DEPLOYER_CONTRACT as string,
-            abi: JSON.stringify(deployerABI),
-            events: [{ name: "wallet_deployed", fn: "handleDeploy", }],
-            // TODO: this one should be dynamic
-            start: 779875,
-        },
-        {
-            // TODO: this one should be dynamic
-            contract: "0x50eb7745e3108d3e5ec6937a83a974929fcc7af94d5eaba7b4acf674c77f7ff",
-            abi: JSON.stringify(accountABI),
-            events: [{ name: "signer_add", fn: "handleSignerAdd", }],
-            // TODO: this one should be dynamic
-            start: 779875,
-        }
-    ]
+  network_node_url: process.env.RPC_URL as string,
+  sources: [
+    {
+      contract: process.env.DEPLOYER_CONTRACT as string,
+      abi: 'Deployer',
+      events: [{ name: 'wallet_deployed', fn: 'handleDeploy' }],
+      start: STARTING_BLOCK,
+    },
+  ],
+  // dynamic template to track all the accounts deployed by the deployer contract
+  templates: {
+    Account: {
+      abi: 'Account',
+      events: [{ name: 'signer_add', fn: 'handleSignerAdd' }],
+    },
+  },
 };
 
 export default config;
